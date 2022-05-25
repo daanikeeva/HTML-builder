348de3ea3ad054e806162551a76dc58a3a2287e4
Darya Anikeeva <daanikeeva@gmail.com>
2022-05-25 20:52:22 +0300

    fix: 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const pathToFile = path.join(__dirname, 'text.txt');
 
-readableStream.on('error', error => console.log('Error', error.message));
 const readableStream = fs.createReadStream(pathToFile, 'utf-8');
+readableStream.on('error', error => console.log('Error', error.message));
 let text = '';
 readableStream.on('data', chunk => text += chunk);
 readableStream.on('end', () => console.log(text));
