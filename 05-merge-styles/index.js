5bb52680b548a705d7439f3329776350732416bf
Darya Anikeeva <daanikeeva@gmail.com>
2022-05-25 20:20:35 +0300

    fix: 05-merge-style

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,11 +1,11 @@
-const { readdir, readFile, appendFile } = require('fs/promises');
+const { readdir, readFile, appendFile, writeFile} = require('fs/promises');
 const path = require('path');
 
 const pathToFiles = path.join(__dirname, 'styles');
 const pathToBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
 async function mergeStyles() {
-  await appendFile(pathToBundle, '');
+  await writeFile(pathToBundle, '');
   const files = await readdir(pathToFiles, { withFileTypes: true});
     for (const file of files) {
       const pathToFile = path.join(`${pathToFiles}`, `${file.name}`);
